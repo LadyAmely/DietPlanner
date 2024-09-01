595c5915cb13dc7fcaa01274f4e93e7eadc58c92
Amelia <nawrotamelia3@gmail.com>
2024-09-02 01:11:55 +0200

    commit

--- frontend/src/RecipeCard.js
+++ frontend/src/RecipeCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./recipe-database.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faUtensils, faBacon, faCalculator } from '@fortawesome/free-solid-svg-icons';
 
 function RecipeCard({title, category, image, prep_time, calories, diet_type}){
     
@@ -7,10 +9,13 @@ function RecipeCard({title, category, image, prep_time, calories, diet_type}){
         <div className="recipe-card">
             <img src={image} alt={title}/>
             <h3>{title}</h3>
-            <p>{category}</p>
-            <p>{calories}</p>
-            <p>{prep_time}</p>
-            <p>{diet_type}</p>
+            <div className="info">
+                <div><i className="fas fa-clock"></i> {prep_time} min</div>
+                <div><i className="fas fa-utensils"></i>{category}</div>
+                <div><i className="fas fa-apple-alt"></i>{diet_type}</div>
+                <div><i className="fas fa-calculator"></i>{calories} kcal</div>
+            </div>
+
             <button className="btn details-btn">Details</button>
         </div>
     );
